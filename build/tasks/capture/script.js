eefcbe806f67d47d44ac3b330d2aab5a93ac9038
Zim <zimtsui@gmail.com>
2023-02-16 17:36:56 +0800

    startable adaptor

--- build/tasks/capture/script.js
+++ build/tasks/capture/script.js
@@ -8,16 +8,35 @@ const success_1 = require("../../async-rpc/executor/success");
 const failure_1 = require("../../async-rpc/executor/failure");
 const execute_1 = require("./execute");
 const startable_adaptor_1 = require("startable-adaptor");
+const startable_1 = require("startable");
 assert(process.env.TASKLIST_HOST_URI);
 assert(process.env.TASKLIST_DB);
 assert(process.env.TASKLIST_COLL);
-const host = new mongodb_1.MongoClient(process.env.TASKLIST_HOST_URI);
-const db = host.db(process.env.TASKLIST_DB);
-const coll = db.collection(process.env.TASKLIST_COLL);
-const stream = coll.watch();
-const adoption = new adoption_1.Adoption(host, db, coll);
-const success = new success_1.Success(host, db, coll);
-const failure = new failure_1.Failure(host, db, coll);
-const executor = new executor_1.Executor(stream, adoption, success, failure, 'capture', execute_1.execute);
-(0, startable_adaptor_1.adapt)(executor.$s);
+class App {
+    constructor() {
+        this.$s = (0, startable_1.createStartable)(this.rawStart.bind(this), this.rawStop.bind(this));
+    }
+    async rawStart() {
+        this.host = new mongodb_1.MongoClient(process.env.TASKLIST_HOST_URI);
+        this.host.on('close', () => void this.$s.stop());
+        this.db = this.host.db(process.env.TASKLIST_DB);
+        this.coll = this.db.collection(process.env.TASKLIST_COLL);
+        this.stream = this.coll.watch();
+        this.stream.on('close', () => void this.$s.stop());
+        this.adoption = new adoption_1.Adoption(this.host, this.db, this.coll);
+        this.success = new success_1.Success(this.host, this.db, this.coll);
+        this.failure = new failure_1.Failure(this.host, this.db, this.coll);
+        this.executor = new executor_1.Executor(this.stream, this.adoption, this.success, this.failure, 'capture', execute_1.execute);
+        await this.executor.$s.start(this.$s.stop);
+    }
+    async rawStop() {
+        if (this.executor)
+            await this.executor.$s.stop();
+        if (this.stream)
+            await this.stream.close();
+        if (this.host)
+            await this.host.close();
+    }
+}
+(0, startable_adaptor_1.adapt)(new App().$s);
 //# sourceMappingURL=script.js.map
--- src/tasks/capture/script.ts
+++ src/tasks/capture/script.ts
@@ -1,6 +1,6 @@
 import { Executor } from "../../async-rpc/executor";
 import assert = require("assert");
-import { MongoClient } from "mongodb";
+import { ChangeStream, Collection, Db, MongoClient } from "mongodb";
 import { Document } from "../../async-rpc/interfaces";
 import { Adoption } from "../../async-rpc/executor/adoption";
 import { Success } from "../../async-rpc/executor/success";
@@ -8,30 +8,57 @@ import { Failure } from "../../async-rpc/executor/failure";
 import { execute } from "./execute";
 import { Method, Params, Result, ErrDesc } from "./interfaces";
 import { adapt } from "startable-adaptor";
+import { createStartable } from "startable";
+
 
 
 assert(process.env.TASKLIST_HOST_URI);
 assert(process.env.TASKLIST_DB);
 assert(process.env.TASKLIST_COLL);
 
+class App {
+	public $s = createStartable(
+		this.rawStart.bind(this),
+		this.rawStop.bind(this),
+	);
+	private executor?: Executor<Method, Params, Result, ErrDesc>;
+	private host?: MongoClient;
+	private db?: Db;
+	private coll?: Collection<Document>;
+	private stream?: ChangeStream<Document>;
 
-const host = new MongoClient(process.env.TASKLIST_HOST_URI);
-const db = host.db(process.env.TASKLIST_DB);
-const coll = db.collection<Document>(process.env.TASKLIST_COLL);
-const stream = coll.watch();
+	private adoption?: Adoption;
+	private success?: Success;
+	private failure?: Failure;
 
-const adoption = new Adoption(host, db, coll);
-const success = new Success(host, db, coll);
-const failure = new Failure(host, db, coll);
+	private async rawStart() {
+		this.host = new MongoClient(process.env.TASKLIST_HOST_URI!);
+		this.host.on('close', () => void this.$s.stop());
+		this.db = this.host.db(process.env.TASKLIST_DB!);
+		this.coll = this.db.collection<Document>(process.env.TASKLIST_COLL!);
+		this.stream = this.coll.watch();
+		this.stream.on('close', () => void this.$s.stop());
 
+		this.adoption = new Adoption(this.host, this.db, this.coll);
+		this.success = new Success(this.host, this.db, this.coll);
+		this.failure = new Failure(this.host, this.db, this.coll);
 
-const executor = new Executor<Method, Params, Result, ErrDesc>(
-	stream,
-	adoption,
-	success,
-	failure,
-	'capture',
-	execute,
-);
+		this.executor = new Executor(
+			this.stream,
+			this.adoption,
+			this.success,
+			this.failure,
+			'capture',
+			execute,
+		);
+		await this.executor.$s.start(this.$s.stop);
+	}
 
-adapt(executor.$s);
+	private async rawStop() {
+		if (this.executor) await this.executor.$s.stop();
+		if (this.stream) await this.stream.close();
+		if (this.host) await this.host.close();
+	}
+}
+
+adapt(new App().$s);
